a06be7ebefb06058a516f27ee47f4c19f154e125
Abiona Michael <amabiona21@gmail.com>
2021-06-01 10:21:18 +0100

    fixed routing problem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Billing from './screens/Billing';
 import Profile from './screens/Profile';
 import Purchase from './screens/Purchase';
@@ -6,14 +6,14 @@ import Success from './screens/Success';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Profile} />
         <Route path="/billing" component={Billing} />
         <Route path="/purchase" component={Purchase} />
         <Route path="/success" component={Success} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
